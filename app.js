a6a5a6e393c330ef6fe8349ae02f6f340e75972c
Kikaikum <enriquecabrera92@gmail.com>
2021-12-29 23:48:29 +0100

    exercici 1

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+const API_URL = "https://icanhazdadjoke.com/";
+function AddJoke() {
+    fetch(API_URL, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(response => {
+        if (response.ok)
+            return response.json();
+    })
+        .then(function (data) {
+        console.log(data.joke);
+    })
+        .catch(err => {
+        console.error("ERROR: ", err.message);
+    });
+}
---
+++ app.ts
@@ -0,0 +1,20 @@
+const API_URL="https://icanhazdadjoke.com/";
+
+
+function AddJoke(){
+    fetch(API_URL, {
+    headers: {
+        'Accept':'application/json'
+    }
+})
+  .then(response => {
+    if (response.ok)        
+        return response.json();    
+  })  
+  .then(function(data) {
+    console.log(data.joke);
+})
+  .catch(err => {
+    console.error("ERROR: ", err.message)
+  });
+}
