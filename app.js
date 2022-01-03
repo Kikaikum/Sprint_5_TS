e3d715691a7d481c6949477e6e615d23afab93cf
Kikaikum <enriquecabrera92@gmail.com>
2022-01-03 12:15:08 +0100

    1.02

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
 const API_URL = "https://icanhazdadjoke.com/";
+const reportAcudits = [];
+const acudit = {};
 function AddJoke() {
     fetch(API_URL, {
         headers: {
@@ -11,9 +13,27 @@ function AddJoke() {
             return response.json();
     })
         .then(function (data) {
-        console.log(data.joke);
+        var chiste = data.joke;
+        const dom = document.getElementById("jokes");
+        dom.innerHTML = chiste;
     })
         .catch(err => {
         console.error("ERROR: ", err.message);
     });
+}
+function Joke(puntos) {
+    var _a;
+    const joke = (_a = document.getElementById("jokes")) === null || _a === void 0 ? void 0 : _a.textContent;
+    const d = new Date();
+    let text = d.toISOString();
+    if (joke != " " && joke) {
+        let acudit = {
+            joke: joke,
+            score: puntos,
+            date: text
+        };
+        reportAcudits.push(acudit);
+        console.log(reportAcudits);
+    }
+    AddJoke();
 }
--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
 const API_URL:string="https://icanhazdadjoke.com/";
 
+const reportAcudits:any=[];
+const acudit={};
+interface Acudit{
+  joke:string;
+  score:number;
+  date:string;
+}
 
 function AddJoke(){
     fetch(API_URL, {
@@ -11,10 +18,35 @@ function AddJoke(){
     if (response.ok)        
         return response.json();    
   })  
-  .then(function(data) {
-    console.log(data.joke);
+  .then(function(data) {    
+    var chiste:string=data.joke;    
+    const dom:HTMLElement=document.getElementById("jokes") as HTMLElement;       
+    dom.innerHTML = chiste;
+    
 })
   .catch(err => {
     console.error("ERROR: ", err.message)
   });
 }
+
+function Joke(puntos:number){
+  const joke=document.getElementById("jokes")?.textContent;
+  const d = new Date();
+  let text = d.toISOString();
+  
+  if(joke !=" " && joke){
+    let acudit: Acudit={
+      joke:joke,
+      score:puntos,
+      date:text
+    };
+    reportAcudits.push(acudit);
+    console.log(reportAcudits);
+  }
+  
+  
+  AddJoke();
+}
+
+
+ 
